dfafc0c300610857b39c7c0089691a65de481f86
Ashraful Alam <aamorroid66@gmail.com>
2025-05-05 16:34:41 +0600

    api route

--- pages/login.vue
+++ pages/login.vue
@@ -1,7 +1,17 @@
 <script setup>
+import { useState } from 'nuxt/app';
+
 const response = await $fetch('/api/hello');
-console.log(response)
+console.log(response);
+const isUserLoggedOut=useState('logout',()=>0);
+const isUserRegistered=useState('register',()=>0);
+
 </script>
 <template>
     <h1>Login</h1>
+    <div>
+   <button v-if="isUserRegistered">Register</button>
+   <button v-else-if="isUserLoggedOut">Login</button>
+   <button v-else>Logout</button>
+ </div>
 </template>
---
+++ server/routes/hello.ts
@@ -0,0 +1,12 @@
+export default defineEventHandler(async (event) => {
+    // ... Do whatever you want here
+    return {
+        api:"works",
+        name:"Sina",
+        address: {
+            home:"mostang villa",
+            street:"jakhrand"
+        }
+    }
+  })
+  
